d8a208482a5f1c7b903f32a2b724165846edff44
yushinliou <vxh440@gmail.com>
2025-12-15 20:36:58 +0100

    feel the way

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   // Uncomment if deploying to a subpath
   // base: '/your-repo-name',
   site: 'https://yushinliou.github.io',
