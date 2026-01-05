980952c4b48a07b6e0c95a51dc0b5715f7111105
yushinliou <vxh440@gmail.com>
2026-01-05 01:42:07 +0100

    social code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import remarkFootnotes from 'remark-footnotes';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    react(),
+    mdx({
+      remarkPlugins: [remarkFootnotes, remarkMath],
+      rehypePlugins: [rehypeKatex],
+    })
+  ],
   // Uncomment if deploying to a subpath
   // base: '/your-repo-name',
   site: 'https://yushinliou.github.io',
